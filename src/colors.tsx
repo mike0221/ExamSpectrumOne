2f98d22036af8cce66f4839446b038a992780105
Michael Macalintal <michael.macalintal@serino.com>
2023-07-06 06:21:08 +0800

    chore: create config for the colors and other configs

---
+++ src/colors.tsx
@@ -0,0 +1,3 @@
+export const primary = '#1e214d';
+export const white = '#f2f3fa';
+export const black = '#0e101c';
---
+++ src/config/config.ts
@@ -0,0 +1,2 @@
+export const GOOGLE_WEB_CLIENT_ID =
+  '986510417796-495rjvjjh10l7oljquj9ohka3mjno32s.apps.googleusercontent.com';
